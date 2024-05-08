b21d3b6aef81dff005c5571c6884db56c8508ed5
Opemipo Adeoyo <108328249+TdepbwT@users.noreply.github.com>
2024-05-08 19:12:13 +0100

    create dictcreator.py
    script to create dictionary of course names and corresponding txt files

---
+++ dictcreator.py
@@ -0,0 +1,16 @@
+import os
+
+def create_dict(folder_path):
+    file_dictionary = {}
+    for filename in os.listdir(folder_path):
+        filepath = os.path.join(folder_path, filename)
+        if os.path.isfile(filepath):
+            with open(filepath, "r") as file:
+                first_line = file.readline().strip()
+                file_dictionary[filename] = first_line
+    return file_dictionary
+
+
+folder_path = input("enter folder path: ")
+result_dict = create_dict(folder_path)
+print(result_dict)
